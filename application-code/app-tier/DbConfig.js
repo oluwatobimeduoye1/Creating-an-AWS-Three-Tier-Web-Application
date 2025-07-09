24110be99ff682d0a4bda46b92b2f45d4756e94a
unknown <oluwatobimeduoye6@gmail.com>
2025-07-09 06:05:10 +0100

    updates

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c1e0cq8mknse.us-west-2.rds.amazonaws.com',
+    DB_USER : 'admin3',
+    DB_PWD : 'Mcfrank-100',
+    DB_DATABASE : 'webappdb'
+});
